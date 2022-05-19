6f713083e25e9d864bf547186934f5417531617e
stevenpershyn <102390494+stevenpershyn@users.noreply.github.com>
2022-05-19 19:15:28 -0400

    Added MongoConfig

---
+++ src/main/java/com/quintrix/carportal/customer/config/MongoConfig.java
@@ -0,0 +1,29 @@
+package com.quintrix.carportal.customer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+@Configuration
+public class MongoConfig {
+
+	@Bean
+	public MongoClient mongo() {
+		ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/customer");
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+          .applyConnectionString(connectionString)
+          .build();
+        
+        return MongoClients.create(mongoClientSettings);
+	}
+	
+	@Bean
+    public MongoTemplate mongoTemplate() throws Exception {
+        return new MongoTemplate(mongo(), "customer");
+    }
+	
+}
