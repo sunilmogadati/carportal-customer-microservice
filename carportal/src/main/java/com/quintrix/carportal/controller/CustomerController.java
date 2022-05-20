bab840956b569a30c108f9d74cb3f5615dfb467f
James Nimmo <Tristan_Nimmo@msn.com>
2022-05-20 12:02:08 -0400

    Added logging.

--- carportal/src/main/java/com/quintrix/carportal/controller/CustomerController.java
+++ carportal/src/main/java/com/quintrix/carportal/controller/CustomerController.java
@@ -1,6 +1,8 @@
 package com.quintrix.carportal.controller;
 
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,44 +11,43 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.quintrix.jfs.customer.documents.Customer;
-import com.quintrix.jfs.customer.models.GetCustomerResponse;
 import com.quintrix.jfs.customer.service.CustomerService;
 
-
+// Need to add error handling somewhere
 @RestController
 public class CustomerController {
+
+  private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
 
   @Autowired
   CustomerService customerService;
 
-  // Format changes
   @RequestMapping(method = RequestMethod.GET, value = "/customers")
   List<Customer> getAllCustomers() {
 
     return customerService.getAllCustomers();
   }
 
+  // Retrieves complete customer information including id.
   @RequestMapping(method = RequestMethod.GET, value = "/customer")
-  GetCustomerResponse getCustomers(@RequestParam(name = "name", required = false) String name) {
+  Customer getCustomers(@RequestParam(name = "name", required = false) String name) {
 
+    logger.debug("Request: Searrch records for name: {}", name);
     return customerService.getCustomers(name);
   }
 
   @RequestMapping(method = RequestMethod.GET, value = "/customer/{id}")
   Customer getCustomerDetails(@PathVariable("id") Long id) {
 
-    if (id % 1 == 0) { // Checking for whole number
-      return customerService.getCustomerById(id);
-    } else {
-      return null;
-    }
+    logger.debug("Request: Called getCustomerById Controller {}", id);
+    return customerService.getCustomerById(id);
 
   }
-
 
   @RequestMapping(method = RequestMethod.POST, value = "/customer")
   Customer addCustomer(@RequestBody Customer customer) {
 
+    logger.debug("Request: Added new customer: {}", customer);
     return customerService.addCustomer(customer);
   }
 }
