07e2a064ceb068fa29a1d4ff1de6463ced7620a1
stevenpershyn <102390494+stevenpershyn@users.noreply.github.com>
2022-05-20 14:26:14 -0400

    Update MongoConfig.java

--- src/main/java/com/quintrix/carportal/customer/config/MongoConfig.java
+++ src/main/java/com/quintrix/carportal/customer/config/MongoConfig.java
@@ -11,19 +11,20 @@ import com.mongodb.client.MongoClients;
 @Configuration
 public class MongoConfig {
 
-	@Bean
-	public MongoClient mongo() {
-		ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/customer");
+    private final String CUSTOMER_REPOSITORY_TEST_NAME = "customer";
+    @Bean
+    public MongoClient mongo() {
+	ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/" + CUSTOMER_REPOSITORY_TEST_NAME);
         MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
           .applyConnectionString(connectionString)
           .build();
         
         return MongoClients.create(mongoClientSettings);
-	}
+    }
 	
-	@Bean
-    public MongoTemplate mongoTemplate() throws Exception {
-        return new MongoTemplate(mongo(), "customer");
+    @Bean
+    	public MongoTemplate mongoTemplate() throws Exception {
+        return new MongoTemplate(mongo(), CUSTOMER_REPOSITORY_TEST_NAME);
     }
 	
 }
