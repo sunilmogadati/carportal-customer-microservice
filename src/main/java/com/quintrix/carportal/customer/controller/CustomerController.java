33b644b09f6439fecbcd45c08578abd351393112
marian-antonio <mra2@usf.edu>
2022-05-20 13:40:25 -0400

    cleaned up code, removed TODO comments

--- src/main/java/com/quintrix/carportal/customer/controller/CustomerController.java
+++ src/main/java/com/quintrix/carportal/customer/controller/CustomerController.java
@@ -2,6 +2,7 @@ package com.quintrix.carportal.customer.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -11,31 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CustomerController {
 
-  // @Autowired
-  // CustomerService service;
+  private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
 
-  Logger logger = LoggerFactory.getLogger(CustomerController.class);
+  @Autowired
+  CustomerService customerService;
 
-  // TODO: get all customers
-
-  // TODO: get one customer info by id or other method
-
-  // TODO: add/register customer
-
-  // TODO: customer log in?
-
-  // update customer: name/email/password/phone/active/address/
   @PutMapping("/customer/update")
   public Customer updateCustomerInformation(@RequestBody Customer customer) {
     logger.debug("Request: update information Customer {}", customer);
-    return service.updateCustomer(customer);
+    return customerService.updateCustomer(customer);
   }
 
-  // delete customer
-  @DeleteMapping("/customers/{id}")
+  @DeleteMapping("/customer/{id}")
   public void deleteCustomerById(@PathVariable Long id) {
     logger.debug("Request: delete Customer with id {}", id);
-    return service.deleteCustomer(id);
+    return customerService.deleteCustomer(id);
   }
-
 }
