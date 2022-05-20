dd9aacc365af6f590a998b1f1deed49ec3a5c794
Hamilton Laws <hamiltonlaws233@gmail.com>
2022-05-20 15:13:55 -0400

    created teh service layer with update and delete for the controller layer.

---
+++ src/main/java/com/quintrix/carportal/customer/service/CustomerService.java
@@ -0,0 +1,8 @@
+package com.quintrix.carportal.customer.service;
+
+public interface CustomerService {
+
+  public Customer updateCustomer(Customer customer);
+
+  public String deleteCustomer(Long id);
+}
