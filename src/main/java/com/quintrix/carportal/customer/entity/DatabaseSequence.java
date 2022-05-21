9b9b6262a665642be51503ab086ef3d64f89b407
Isaac Terry <zeroice2007@gmail.com>
2022-05-20 19:16:26 -0500

    Add additional comments

--- src/main/java/com/quintrix/carportal/customer/entity/DatabaseSequence.java
+++ src/main/java/com/quintrix/carportal/customer/entity/DatabaseSequence.java
@@ -2,6 +2,9 @@ package com.quintrix.carportal.customer.entity;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+// The purpose of this class it to store sequences so that the customer id will be able to be auto
+// incremented using a stored sequence
+
 // NOTE: this class came from
 // https://github.com/eugenp/tutorials/blob/master/persistence-modules/spring-boot-persistence-mongodb/src/main/java/com/baeldung/mongodb/models/DatabaseSequence.java
 @Document
