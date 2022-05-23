ef5dbcd7fc77306cfcdc6b256fa1d725237be79c
Brandon Northrop <brannorth0414@yahoo.com>
2022-05-23 17:10:18 -0600

    Create ClientCustomer.java

---
+++ src/main/java/com/quintrix/carportal/customer/entity/ClientCustomer.java
@@ -0,0 +1,58 @@
+package com.quintrix.carportal.customer.entity;
+
+import java.util.List;
+
+public class ClientCustomer {
+
+  private String name;
+
+  private String email;
+
+  private String phoneNumber;
+
+  private String address;
+
+  private List<String> ownedCars;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public List<String> getOwnedCars() {
+    return ownedCars;
+  }
+
+  public void setOwnedCars(List<String> ownedCars) {
+    this.ownedCars = ownedCars;
+  }
+
+}
+
